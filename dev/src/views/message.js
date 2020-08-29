6e01ef0f6d9d18d0df2a2b97768809face6aa06f
Sefhi <sefhi@MacBook-Pro-de-Paula.local>
2020-08-29 16:15:44 +0100

    updated comments and reformatted some of the code

--- dev/src/views/message.js
+++ dev/src/views/message.js
@@ -1,37 +1,55 @@
 import html from './message.html';
 import './message.css';
 
-let body
-let tmp = document.createElement('div')
-tmp.innerHTML = html
-let container = tmp.querySelector('.whtsc')
+let body;
+let tmp = document.createElement('div');
+tmp.innerHTML = html;
+let container = tmp.querySelector('.whtsc');
 
 /* Initial configuration for the widget */
 export function config(config) {    
-    if(config.message) {// Welcome message
-        // If we use cookies, check to see if the cookie exists already or check if we don't use cookies
+	// Welcome message
+    if(config.message) {
+        // If we use cookies, check to see if the cookie exists already
         if(config.useCookies && document.cookie.match(/^(.*;)?\s*showmessage\s*=\s*[^;]+(.*)?$/) == null ) {
-            container.querySelector('.whtsm').innerHTML = `${config.message} <span></span>`
-            container.querySelector('.whtsm span').addEventListener('click', closeMessage) // Event listener to close the message on the widget
-            createCookie('showmessage', 'no', 7) // Create the cookie
-        } else if (!config.useCookies) { // Show the message if we're not using cookies always
-            container.querySelector('.whtsm').innerHTML = `${config.message} <span></span>`
-            container.querySelector('.whtsm span').addEventListener('click', closeMessage) // Event listener to close the message on the widget
-        } else // Remove the div that contains the welcome message if we're using the cookie and it already exists
-            container.querySelector('.whtsm').remove()
-    } else // If there's no welcome message, remove that div
-        container.querySelector('.whtsm').remove()
-        
-    if(config.phone) // Add whatsapp link
-        container.querySelector('.whatswidget-button').href = `https://api.whatsapp.com/send?phone=${config.phone}`
-    if(config.floatLeft) // Show the widget on the left side of the page
-        container.classList.add('left')
-    if(config.showMessageAlways) // Show the widget message on mobile devices
-        container.classList.add('show-message-always')
+			container.querySelector('.whtsm').innerHTML = `${config.message} <span></span>`;
+			// Event listener to close the message on the widget
+			container.querySelector('.whtsm span').addEventListener('click', closeMessage);
+			// Create the cookie
+			createCookie('showmessage', 'no', 7);
+		// Show the message if we're not using cookies
+        } else if (!config.useCookies) {
+			container.querySelector('.whtsm').innerHTML = `${config.message} <span></span>`;
+			// Event listener to close the message on the widget
+			container.querySelector('.whtsm span').addEventListener('click', closeMessage);
+		// Remove the div that contains the welcome message if we're using the cookie and it already exists
+        } else {
+            container.querySelector('.whtsm').remove();
+		}
+	// If there's no welcome message, remove that div
+    } else {
+        container.querySelector('.whtsm').remove();
+	}
+		
+	// Add whatsapp link
+    if(config.phone) {
+		// TODO -> add support for multiple phone numbers and message a random one from the list
+        container.querySelector('.whatswidget-button').href = `https://api.whatsapp.com/send?phone=${config.phone}`;
+	}
 
+	// Show the widget on the left side of the page
+    if(config.floatLeft) {
+        container.classList.add('left');
+	}
 
-    body = document.getElementsByTagName('body')[0]
-    body.appendChild(container)
+	// Show the widget message on mobile devices
+    if(config.showMessageAlways) {
+        container.classList.add('show-message-always');
+	}
+
+	// Add the widget to the body of your page
+    body = document.getElementsByTagName('body')[0];
+    body.appendChild(container);
 }
 
 /* Close widget welcome message */
@@ -42,14 +60,18 @@ export function closeMessage() {
 /* Function to create a cookie to show the message only once each day */
 /* Taken from here https://stackoverflow.com/questions/6561687/how-can-i-set-a-cookie-to-expire-after-x-days-with-this-code-i-have */
 function createCookie(name, value, days) {
-    console.log('creating cookie')
-    let date, expires
-    if (days) {
-        date = new Date()
-        date.setTime(date.getTime()+(days*24*60*60*1000))
-        expires = "; expires="+date.toGMTString()
-    } else 
-        expires = ""
+	console.log('creating cookie');
+	
+	let date;
+	let expires;
 
-    document.cookie = name+"="+value+expires+"; path=/"
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires="+date.toGMTString();
+    } else {
+        expires = "";
+	}
+
+    document.cookie = name+"="+value+expires+"; path=/";
 }
